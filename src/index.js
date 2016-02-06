9e32cf631fb7b9f01dcac4154370a56091ea7e7d
Jason Miller <jason@developit.ca>
2016-02-05 21:57:17 -0500

    Show that structural mutations trigger the same behavior under React

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 
 
-// Preact
 /*
+// Preact
 import { h, render, Component } from 'preact';
 window.h = h;
 class App extends Component {
+	componentDidMount() {
+		setTimeout( () => this.setState({ foo:'bar' }), 5000);
+	}
 	render() {
 		return (
 			<div>
 			  <ons-page>
 			    <ons-toolbar>
-			      <div className="center">Title</div>
+			      <div class="center navigation-bar__center navigation-bar__title">Title</div>
 			    </ons-toolbar>
 			    <p>
 			      <ons-button modifier="light">Push</ons-button>
@@ -28,22 +31,45 @@ class App extends Component {
 render(<App />, document.body);
 */
 
+
 // React
 import { createElement, Component } from 'react';
 import ReactDOM from 'react-dom';
 window.h = createElement;
 class App extends Component {
+	state = {};
+
+	componentDidMount() {
+		setInterval( () => {
+			this.setState({
+				count: (this.state.count || 0) + 1
+			});
+		}, 1000);
+	}
+
 	render() {
+		let { count } = this.state,
+			alt = !count%2;
 		return (
 			<div>
-			  <ons-page>
-			    <ons-toolbar>
-			      <div className="center">Title</div>
-			    </ons-toolbar>
-			    <p>
-			      <ons-button modifier="light">Push</ons-button>
-			    </p>
-			  </ons-page>
+				<ons-page>
+					<ons-toolbar>
+						{ alt ? (
+							<div className="center">Title</div>
+						) : (
+							<p className="center">Title alt</p>
+						) }
+					</ons-toolbar>
+					{ alt ? (
+						<p>
+							<ons-button modifier="light">Push</ons-button>
+						</p>
+					) : (
+						<div>
+							<ons-button modifier="light">Push Alt</ons-button>
+						</div>
+					) }
+				</ons-page>
 			</div>
 		);
 	}
